f88b0421c815aee00eea1cae7db2fd60f7003807
Julius Glaser <mfqb102h@woody3.rrze.uni-erlangen.de>
2025-02-13 12:36:32 +0100

    Added more comments

--- code/laser/training/train.py
+++ code/laser/training/train.py
@@ -61,6 +61,7 @@ def train(network_parameters: NP,
 
     """
     
+    # initialize values and set model to train
     model.train()
     scalingMatrix = torch.tensor(scalingMatrix, device=device)
     HALF_LOG_TWO_PI = 0.91893
@@ -76,6 +77,7 @@ def train(network_parameters: NP,
         clean_t = clean_t.type(torch.FloatTensor).to(device)
         optim.zero_grad()
 
+        # denoise inputs, and calculate loss
         if network_parameters.model == 'DAE':
             recon_t = model(noisy_t)
             loss = loss_function(recon_t, clean_t)
@@ -95,6 +97,7 @@ def train(network_parameters: NP,
             recon_loss += mse.item()
             mse_train += mse.item()
 
+        # backpropagation, optimizing and print and 
         loss.backward()
         optim.step()
         train_loss += loss.item()
@@ -104,9 +107,8 @@ def train(network_parameters: NP,
                 100. * batch_idx / len(loader_train),
                 loss.item() / len(noisy_t)))
             
+    # save loss        
     Losses.train.append(train_loss / len(loader_train.dataset)) 
-    #Losses.D.append(D_loss / len(loader_train.dataset))
-
     if network_parameters.model == 'VAE':
         Losses.kld.append(kld_loss / len(loader_train.dataset))
         Losses.recon.append(recon_loss / len(loader_train.dataset))
@@ -144,17 +146,12 @@ def test(network_parameters: NP,
 
     """
     
+    # initialize values and run as testing
     model.eval()
     kld_loss = 0.0
     recon_loss = 0.0
     mse_loss = 0.0
     test_loss = 0.0
-
-    test_clean = []
-    test_noisy = []
-    test_noise_amount = []
-    test_recon = []
-    D_clean_values = []
 
     with torch.no_grad():
         for batch_idx, (noisy_t, clean_t, D_clean, noise) in enumerate(loader_test):
@@ -162,6 +159,7 @@ def test(network_parameters: NP,
             noisy_t = noisy_t.type(torch.FloatTensor).to(device)
             clean_t = clean_t.type(torch.FloatTensor).to(device)
 
+            # denoise inputs and calculate loss (KLD irrelevant and irritating therefore not correctly handled)
             if network_parameters.model == 'DAE':
                 recon_t = model(noisy_t)
                 loss = loss_function(recon_t, clean_t)
@@ -174,13 +172,14 @@ def test(network_parameters: NP,
 
                 kld_loss += (network_parameters.kld_weight*KLD).item()
                 recon_loss += loss_lossf.item()
-                loss = loss_lossf*32 + network_parameters.kld_weight*KLD
+                loss = loss_lossf + network_parameters.kld_weight*KLD
 
 
             loss_mse = ae.loss_function_mse(recon_t, clean_t)
             mse_loss += loss_mse.item()
             test_loss += loss.item()
 
+    # append losses to LC
     Losses.test.append(test_loss / len(loader_test.dataset))
     Losses.mse.append(mse_loss / len(loader_test.dataset))
 
@@ -196,7 +195,7 @@ def test(network_parameters: NP,
     print('====> Epoch: {} Average loss Testing: {:12.7f}'.format(epoch, Losses.test[-1]))
     print('====> Epoch: {} Average mse_loss Testing: {:12.7f}'.format(epoch, Losses.mse[-1]))
 
-    #save trained model
+    #save current model state
     out_str=''
     if epoch == network_parameters.epochs:
         out_str = '/train_' + network_parameters.model + '_Latent' + str(network_parameters.latent).zfill(2) + 'final.pt'
@@ -217,20 +216,16 @@ def setup(ACQ_DIR: str) -> tuple[NP, np.array, np.array, np.array, np.array, np.
     """
     Setup function for the training script, loads configuration file for training and initializes all return variables:
 
-    #TODO:!!!
     Args:
-        network_parameters (NP): an instance of the NetworkParameters util_classes, 
-        loader_test (data.DataLoader): neural network data loader for training data, 
-        optim (torch.optim.optimizer): optimizer, 
-        model (torch.nn.Module): model to be trained, 
-        device (torch.device): device for training (cpu or cuda), 
-        loss_function (torch.nn.modules.loss): loss function for reconstruction loss, 
-        epoch (int): current training epoch, 
-        Losses (LC): an instance of the Loss_class util_classes
+        ACQ_DIR (str): path to the directory containing yaml config and where to store data, 
 
     Returns:
-        LC: Losses with added current test loss.
-
+        NP: network_parameters instance, storing all sorts of important parameters for training and set according to yaml config,
+        np.array: simulated clean diffusion data,
+        np.array: simulated clean diffusion data coefficents depending on model (BAS: stick coordinates, DTI: 3 eigenvectors of tensor),
+        np.array: B matrix from b values and g vectors,
+        np.array: b0_mask, array where b0 entries of sequence b-values 0, all others 1, to later adapt the network architecture accordingly
+        np.array: scaling_matrix, shape of bvals and certain factor for everyone, used to scale loss terms
     """
 
     stream = open(ACQ_DIR + '/config.yaml', 'r')
@@ -240,13 +235,9 @@ def setup(ACQ_DIR: str) -> tuple[NP, np.array, np.array, np.array, np.array, np.
 
     print(NetworkParameters)
 
-    #TODO: different definition here!
-    if NetworkParameters.N_diff == 126:
-        f = h5py.File(r'../raw-data/data-126-dir/1.0mm_126-dir_R3x3_dvs.h5', 'r')
-    elif NetworkParameters.N_diff == 32:
-        f = h5py.File(ACQ_DIR + '/diff_encoding.h5', 'r')
-    elif NetworkParameters.N_diff == 21:
-        f = h5py.File('/home/hpc/iwbi/iwbi019h/DeepSubspaceMRI/deepsubspacemri/data_files/4shot_data/1.0mm_21-dir_R1x3_dvs.h5', 'r')
+    # load sequence info here, which stores bvals and gradient vectors
+    # if different than usual data is used, please adapt here
+    f = h5py.File('../../../data/raw/1.0mm_126-dir_R3x3_refs.h5', 'r')
 
     bvals = f['bvals'][:]
     bvecs = f['bvecs'][:]
@@ -258,6 +249,7 @@ def setup(ACQ_DIR: str) -> tuple[NP, np.array, np.array, np.array, np.array, np.
     b0_threshold = 50
     b0_mask = bvals > b0_threshold
 
+    # simulate data using models 
     if NetworkParameters.diff_model == 'DTI':
         x_clean, original_D = dwi.model_DTI(bvals, bvecs, b0_threshold, 18, None)  #bvals = b, bvecs = g, returns y_pick
         original_D = original_D.T
@@ -276,6 +268,23 @@ def setup(ACQ_DIR: str) -> tuple[NP, np.array, np.array, np.array, np.array, np.
 def create_noised_dataset(train_set: dataset.qSpaceDataset, 
                           test_set: dataset.qSpaceDataset, 
                           NetworkParameters: NP) -> tuple[dataset.qSpaceDataset, dataset.qSpaceDataset]:
+    """
+    creates noisy versions of the handed over clean datasets. 
+    Split of train and test set is done before to make the test samples completely unknown to the network in testing:
+
+    Args:
+        train_set (dataset.qSpaceDataset): train set for the network, 
+        test_set (dataset.qSpaceDataset): test set for the network, 
+        NetworkParameters (NP): network parameters storing important network information 
+
+    Returns:
+        NP: network_parameters instance, storing all sorts of important parameters for training and set according to yaml config,
+        np.array: simulated clean diffusion data,
+        np.array: simulated clean diffusion data coefficents depending on model (BAS: stick coordinates, DTI: 3 eigenvectors of tensor),
+        np.array: B matrix from b values and g vectors,
+        np.array: b0_mask, array where b0 entries of sequence b-values 0, all others 1, to later adapt the network architecture accordingly
+        np.array: scaling_matrix, shape of bvals and certain factor for everyone, used to scale loss terms.
+    """
 
     train_set_full = dc(train_set)
     test_set_full = dc(test_set)
@@ -284,8 +293,10 @@ def create_noised_dataset(train_set: dataset.qSpaceDataset,
         train_set_copy = dc(train_set)
         test_set_copy = dc(test_set)
 
+        # standard deviation of noise to be added, can be adjusted
         sd = 0.01 + id * 0.03
 
+        # add noise to the copies
         for index in range(len(train_set_copy)):
             train_set_copy[index][0][:] = dataset.add_noise(train_set_copy[index][1], sd, NetworkParameters.noise_type)
             train_set_copy[index][3][:] = id
@@ -294,18 +305,33 @@ def create_noised_dataset(train_set: dataset.qSpaceDataset,
             test_set_copy[index][0][:] = dataset.add_noise(test_set_copy[index][1], sd, NetworkParameters.noise_type)
             test_set_copy[index][3][:] = id
 
+        # append noised versions to complete dataset
         train_set_full = data.ConcatDataset([train_set_full, train_set_copy])  
         test_set_full =  data.ConcatDataset([test_set_full, test_set_copy]) 
-    print('train set size wo noise', len(train_set))
-    print('test set size wo noise', len(test_set))
-    print('train set size with noise', len(train_set)*NetworkParameters.noise_range)
-    print('test set size with noise', len(test_set)*NetworkParameters.noise_range)
+
+    print('>>train set size without noise', len(train_set))
+    print('>>test set size without noise', len(test_set))
+    print('>>train set size with noise', len(train_set)*NetworkParameters.noise_range)
+    print('>>test set size with noise', len(test_set)*NetworkParameters.noise_range)
 
     return train_set_full, test_set_full
 
 def create_data_loader(x_clean: np.array, 
                        original_D: np.array, 
                        NetworkParameters: NP) -> tuple[data.DataLoader, data.DataLoader]:
+    """
+    creates noisy versions of the handed over clean datasets. 
+    Split of train and test set is done before to make the test samples completely unknown to the network in testing:
+
+    Args:
+        x_clean (dataset.qSpaceDataset): clean simulated diffusion data, 
+        original_D (dataset.qSpaceDataset): simulated clean diffusion data coefficents depending on model (BAS: stick coordinates, DTI: 3 eigenvectors of tensor),
+        NetworkParameters (NP): network parameters storing important network information 
+
+    Returns:
+        data.DataLoader: torch dataloader for training data to train network,
+        data.DataLoader: torch dataloader for testing data to test network,
+    """
 
     noise_amount = np.zeros(shape=(1, x_clean.shape[1]))
     noise_amount_full = np.transpose(noise_amount)
@@ -314,52 +340,55 @@ def create_data_loader(x_clean: np.array,
     x_no_noise_yet = dc(x_clean)
     original_D = np.transpose(original_D)
 
-    print('shape x_clean = ', x_clean.shape)
-    print('shape x_no_noise_yet = ', x_no_noise_yet.shape)
-    print('shape original_D = ', original_D.shape)
-    print('shape noise_amount_full = ', noise_amount_full.shape)
-    q_dataset = dataset.qSpaceDataset(x_clean, x_no_noise_yet, original_D, noise_amount_full)
+    print('>>shape x_clean = ', x_clean.shape)
+    print('>>shape x_no_noise_yet = ', x_no_noise_yet.shape)
+    print('>>shape original_D = ', original_D.shape)
+    print('>>shape noise_amount_full = ', noise_amount_full.shape)
 
+    # creates qSpaceDataset from simulated data, inherited from torch dataset and adding functionality
+    q_dataset = dataset.qSpaceDataset(x_clean, x_no_noise_yet, original_D, noise_amount_full)
     q_datalen = len(q_dataset)
 
+    # 80/20 % split of dataset for train/test set
     train_siz = int(q_datalen * 0.8)
     test_siz = q_datalen - train_siz
-
     train_set, test_set = data.random_split(q_dataset, [train_siz, test_siz])  
 
+    # add noise to datasets
     train_set_noised, test_set_noised = create_noised_dataset(train_set, test_set, NetworkParameters)    
 
     loader_train = data.DataLoader(train_set_noised, batch_size=NetworkParameters.batch_size_train, shuffle=True)
-
+    # test set not to be shuffeld to be able to compare results afterwards
     loader_test = data.DataLoader(test_set_noised, batch_size=NetworkParameters.batch_size_test, shuffle=False)
 
     return loader_train, loader_test
 
-
 def main():
+    """
+    main script to train and test network.
+    """
 
     torch.manual_seed(0)
 
+    # reading in path to directory, where to store data and find config yaml
     given_dir = sys.argv[1] if len(sys.argv) > 1 else print('No directory handed over!')
-
     print(f'> directory: {given_dir}\n')
     ACQ_DIR = given_dir
 
+    # Setup training and testing data as well as network and training parameters and file to store losses
     NetworkParameters, x_clean, original_D, B, b0_mask, scalingMatrix = setup(ACQ_DIR)
     device = NetworkParameters.selectDevice()
     model = NetworkParameters.createModel(None, NetworkParameters.N_diff, device)
     loader_train, loader_test = create_data_loader(x_clean, original_D, NetworkParameters)
-
-    
-    
     loss_function = NetworkParameters.selectLoss()
     optimizer = NetworkParameters.selectOptimizer(model)
+    Losses = LC(NetworkParameters.model)
 
+    # file to store testing signals and denoised outputs over epochs
     f = h5py.File(ACQ_DIR + '/valid_' + NetworkParameters.model + '_Latent' + str(NetworkParameters.latent).zfill(2) + 'EpochTrain' + str(NetworkParameters.epochs) + '.h5', 'w')
 
-    Losses = LC(NetworkParameters.model)
-    # %% training
     for epoch in range(1, NetworkParameters.epochs+1, 1):
+        # training of netowrk with adaptive KLD update (see train method for more info)
         gamma_x = np.sqrt(Losses.mse_train)
         Losses = train(NetworkParameters, loader_train, optimizer, model, device, loss_function, epoch, Losses, gamma_x, scalingMatrix)
         
@@ -370,7 +399,9 @@ def main():
         if NetworkParameters.model == 'VAE':
             NetworkParameters.update_kld_weight(epoch)
 
+    # create file, where all kind of losses during training and testing are stored
     Losses.create_loss_file(NetworkParameters)
+    # create mse loss file, shows the mse loss after testing for all checkpoints and the network architecture
     Losses.create_mse_loss_txt_file(NetworkParameters, model)
 
     f.close()
